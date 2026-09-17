791a00626b1f6f6520b28ac1aa8da1422bc242b0
SAKSHAM SHEORAN <saksham3392.beai24@chitkara.edu.in>
2026-09-17 07:00:33 +0530

    Add files via upload

---
+++ PA Q/app.py
@@ -0,0 +1,7 @@
+# Alias entrypoint for Render and local servers
+from server import app
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port, debug=True)
